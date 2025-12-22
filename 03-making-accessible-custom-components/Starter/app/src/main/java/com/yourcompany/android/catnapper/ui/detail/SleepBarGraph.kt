40f27650a73a8caceebf7b66bbbb98a80b1129a9
prajwal27 <prajwalpspbelagavi@gmail.com>
2025-12-22 15:21:34 +0530

    Modified License content in chapter 3.

--- 03-making-accessible-custom-components/Starter/app/src/main/java/com/yourcompany/android/catnapper/ui/detail/SleepBarGraph.kt
+++ 03-making-accessible-custom-components/Starter/app/src/main/java/com/yourcompany/android/catnapper/ui/detail/SleepBarGraph.kt
@@ -1,5 +1,31 @@
 /*
- * Copyright (c) 2024 Your Company. All rights reserved.
+ * Copyright (c) 2025 Kodeco Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 package com.yourcompany.android.catnapper.ui.detail
@@ -12,22 +38,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
+import com.yourcompany.android.catnapper.R
 import com.yourcompany.android.catnapper.data.Nap
 import com.yourcompany.android.catnapper.ui.theme.CatNapperTheme
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 
 @Composable
 fun SleepBarGraph(naps: List<Nap>, modifier: Modifier = Modifier) {
   Layout(
     content = {
-      naps.forEach { _ ->
-        Box(
-          modifier = Modifier
-            .background(MaterialTheme.colors.primary)
+      naps.forEach { nap ->
+        val formatter = DateTimeFormatter.ofPattern("h:mm a")
+        val description = stringResource(
+          id = R.string.nap_content_description,
+          nap.start.format(formatter),
+          nap.end.format(formatter)
         )
+        Box(modifier = Modifier
+          .background(MaterialTheme.colors.primary)
+          .semantics {
+            contentDescription = description
+          })
       }
     },
     modifier = modifier.background(Color.LightGray)
